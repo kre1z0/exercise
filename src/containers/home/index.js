2446b4219a51dc5e7b9e83714a122c60f583ee83
kreiz0 <thekreizo@gmail.com>
2017-07-03 16:37:26 +0300

    sort

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,27 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import groupBy from 'lodash/groupBy';
-import journals from '../../assets/data/journal.json';
-import { natSort } from '../../helpers/sort';
-
-const DateBlock = ({ date, children }) =>
-    <div className="date-block">
-        <div style={{ color: 'red' }} className="inspections-item-date">
-            {date}
-        </div>
-        {children}
-    </div>;
-
-const TaskItem = ({ name, id }) => {
-    return (
-        <div className="date-block">
-            <span className="inspections-item-date">
-                {name}
-            </span>
-            <span> - {id}</span>
-        </div>
-    );
-};
 
 class Home extends Component {
     componentDidMount() {
@@ -29,37 +7,15 @@ class Home extends Component {
     }
 
     render() {
-        const tasks = groupBy(journals, 'year');
-        const nope = groupBy(journals, 'name');
-        const keysTasks = tasks && Object.keys(tasks).sort((a, b) => b - a);
-        console.log('keysTasks', keysTasks);
-        const xuy = nope && Object.keys(nope);
-        console.log('xuy', xuy);
         return (
             <div>
-                {keysTasks.map(date => {
-                    const tasksInDate = tasks[
-                        date
-                    ].sort(({ name: a }, { name: b }) => natSort(a, b));
-                    console.log('tasksInDate', tasksInDate);
-                    return (
-                        <DateBlock date={date} key={date + '-key'}>
-                            {' '}
-                            {tasksInDate &&
-                                tasksInDate.map((task, i) =>
-                                    <TaskItem key={i + '-task'} {...task} />,
-                                )}
-                        </DateBlock>
-                    );
-                })}
+                home
             </div>
         );
     }
 }
 
-const mapProps = ({ twitch }) => ({
-    twitch,
-});
+const mapProps = ({ twitch }) => ({});
 
 const mapActions = {};
 
--- src/containers/sorting/index.js
+++ src/containers/sorting/index.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Grid, Row, Col } from 'react-flexbox-grid';
 import groupBy from 'lodash/groupBy';
 import journals from '../../assets/data/journal.json';
 import { natSort } from '../../helpers/sort';
-import { Button } from 'reactstrap';
 
-const DateBlock = ({ date, children }) =>
-    <div className="date-block">
-        <div style={{ color: 'red' }} className="inspections-item-date">
-            {date}
+const YearBlock = ({ year, children }) =>
+    <div>
+        <div style={{ color: 'red' }}>
+            {year}
         </div>
         {children}
     </div>;
 
-const TaskItem = ({ name, id }) => {
+const IssueItem = ({ name, id, index }) => {
     return (
         <div className="date-block">
             <span className="inspections-item-date">
                 {name}
             </span>
             <span> - {id}</span>
+            <span style={{ color: 'green' }}> - {index}</span>
         </div>
     );
 };
@@ -29,33 +28,27 @@ class Sorting extends Component {
     render() {
         const tasks = groupBy(journals, 'year');
         const keysTasks = tasks && Object.keys(tasks).sort((a, b) => b - a);
-        console.log('keysTasks', keysTasks);
         return (
-            <Grid fluid>
-                <Row>
-                    <Col xs={6} md={3}>
-                        <Button color="danger">Danger!</Button>
-                        {keysTasks.map(date => {
-                            const tasksInDate = tasks[
-                                date
-                            ].sort(({ name: a }, { name: b }) => natSort(a, b));
-                            console.log('tasksInDate', tasksInDate);
-                            return (
-                                <DateBlock date={date} key={date + '-key'}>
-                                    {' '}
-                                    {tasksInDate &&
-                                        tasksInDate.map((task, i) =>
-                                            <TaskItem
-                                                key={i + '-task'}
-                                                {...task}
-                                            />,
-                                        )}
-                                </DateBlock>
-                            );
-                        })}
-                    </Col>
-                </Row>
-            </Grid>
+            <div>
+                {keysTasks.map(date => {
+                    const tasksInDate = tasks[
+                        date
+                    ].sort(({ name: a }, { name: b }) => natSort(a, b));
+                    return (
+                        <YearBlock year={date} key={date + '-key'}>
+                            {' '}
+                            {tasksInDate &&
+                                tasksInDate.map((task, i) =>
+                                    <IssueItem
+                                        index={i + 1}
+                                        key={i + '-task'}
+                                        {...task}
+                                    />,
+                                )}
+                        </YearBlock>
+                    );
+                })}
+            </div>
         );
     }
 }
